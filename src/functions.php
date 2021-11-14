2f5591a54048671553bf29ebc73690bfa2eb46c0
Will Leighton <will@fifteenten.co.uk>
2021-11-14 19:46:45 +0000

    delete removed callback

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,6 @@
 <?php 
 
 
-require_once plugin_dir_path( __FILE__ ) . '/functions/fifteenten_custom_admin_page_contents.php';
 require_once plugin_dir_path( __FILE__ ) . '/functions/search.php';
 require_once plugin_dir_path( __FILE__ ) . '/functions/output.php';
 
