9704d1be63675559a9a59bf93f471b023f923225
Will Leighton <will@fifteenten.co.uk>
2021-11-19 11:21:08 +0000

    pre autoload move

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -29,8 +29,8 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '841848a3a773c34733499ea98830b5949d1fab7f',
-    'name' => 'fifteenten/fifteenten-custom-admin',
+    'reference' => '5b160754d0407cd856771b03d3b030328436748d',
+    'name' => 'willnjl/fifteenten-custom-admin',
   ),
   'versions' => 
   array (
@@ -42,15 +42,6 @@ private static $installed = array (
       array (
       ),
       'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
-    ),
-    'fifteenten/fifteenten-custom-admin' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '841848a3a773c34733499ea98830b5949d1fab7f',
     ),
     'roundcube/plugin-installer' => 
     array (
@@ -65,6 +56,15 @@ private static $installed = array (
       array (
         0 => '*',
       ),
+    ),
+    'willnjl/fifteenten-custom-admin' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5b160754d0407cd856771b03d3b030328436748d',
     ),
   ),
 );
