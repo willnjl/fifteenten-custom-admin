5ef5b7bb2156f904307d5923787bf1eed79595f0
Will Leighton <will@fifteenten.co.uk>
2021-12-10 13:34:00 +0000

    move popup into js

--- assets/js/fifteenten_cookieconsent.js
+++ assets/js/fifteenten_cookieconsent.js
@@ -1,3 +1,86 @@
+const html_popup = `
+        <div>
+            <div class="cc__wrap">
+                <h2>
+                    Cookie Information 🍪
+                </h2>
+                <p>
+                    We use cookies, just like (almost) everyone else, to improve our understanding of how to improve our own website for our visitors.
+                </p>
+                <p>
+                    We want to make sure we're providing the most informative and best arranged experience for our visitors, so we deploy a handful of industry-standard cookies to do so.
+                </p>
+                <p>
+                    Information in these cookies is available in our <a href="/cookies-policy" class="link">Cookies Policy</a> . Please note, our site is unlikely to function without ‘necessary’ cookies (same as other sites requiring cookies for aspects of their functionality). But you can choose whether or not to opt into marketing cookies below.
+                </p>
+                <ul>
+                    <li class="cc__preference_container">
+                        <div class="cc__toggle_holder cc__toggle--essential cc__toggle--active">
+                            <div class="cc__toggle_circle cc__toggle--active"></div>
+                        </div>
+                        <label for="pref_essential">
+                           <strong>Necessary Cookies</strong>  - These are the cookies that are required to make the website work
+                        </label>
+                    </li>
+                    <li class="cc__preference_container">
+                        <div class="cc__toggle_holder cc__toggle--preference" data-preference="marketing">
+                            <div class="cc__toggle_circle"></div>
+                            <input type="checkbox" class="cc__checkbox" name="pref_analytics" >
+                        </div>
+                         <label for="pref_analytics">
+                           <strong>Marketing Cookies</strong> - These are the cookies that are required for us to learn how to improve the experience of our website visitors
+                        </label>
+                    </li>
+                </ul>
+                <div class="cc__btn-container">
+                    <button
+                    id="ButtonCUpdate"
+                    class="cc__btn cc_btn--update"
+                    >
+                        Update to My Selection
+                    </button>
+                    <button
+                    id="ButtonCAccept"
+                    value="Agree to All & Proceed"
+                    class="cc__btn cc__btn--accept"
+                    >
+                        Agree to All & Proceed
+                    </button>
+                    
+                </div>
+            </div>
+        </div>`;
+
+class Popup {
+  constructor(html) {
+    this.popup = this.create(html);
+  }
+
+  create(html) {
+    let popup = document.createElement("div");
+    popup.setAttribute("id", "cookie-popup");
+    popup.innerHTML = html;
+    return document.body.appendChild(popup);
+  }
+
+  get() {
+    return this.popup;
+  }
+  show() {
+    this.popup.style.opacity = 1;
+  }
+  close() {
+    this.popup.style.transform = `translateX(-200%)`;
+    this.popup.style.opacity = 0;
+    setTimeout(() => {
+      this.popup.style.display = "none";
+    }, 1000);
+  }
+  remove() {
+    this.popup.style.display = "none";
+  }
+}
+
 ((d, w) => {
   const storageType = localStorage; // Set Consent Storage Location
 
@@ -49,22 +132,6 @@
     },
   };
 
-  const managePopup = {
-    show: (popup) => {
-      popup.style.opacity = 1;
-    },
-    close: (popup) => {
-      popup.style.transform = `translateX(-200%)`;
-      popup.style.opacity = 0;
-      setTimeout(() => {
-        popup.style.display = "none";
-      }, 1000);
-    },
-    remove: (popup) => {
-      popup.style.display = "none";
-    },
-  };
-
   const cleanUpStorage = (domain) => {
     let regex = /^(_h|_g)/;
 
@@ -93,8 +160,8 @@
   };
 
   let appendGtm = (containerId) => {
-    let AnalyticsData = document.createElement("script");
-    AnalyticsData.text = ` (function (w, d, s, l, i) {
+    let AnalyticsData = d.createElement("script");
+    AnalyticsData.text = `(function (w, d, s, l, i) {
       w[l] = w[l] || [];
       w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
       var f = d.getElementsByTagName(s)[0],
@@ -112,7 +179,6 @@
 
   let postDecline = (popup, state) => {
     state.api.waiting = true;
-
     let promise = axios.post(rest.url + "/decline", {
       data: {
         _wpnonce: rest.nonce,
@@ -120,16 +186,14 @@
     });
     promise.then(({ data, status }) => {
       if (status === 200) {
-        managePopup.close(popup);
         state.api.waiting = !state.api.waiting;
-        console.log(state);
+        popup.close();
       }
     });
 
     promise.catch((error) => {
       state.api.waiting = !state.api.waiting;
       state.api.error = !state.api.error;
-      console.log(state);
     });
   };
 
@@ -138,59 +202,54 @@
     updateConsentGA(status);
     if (status) {
       appendGtm(containerId);
-      managePopup.close(popup);
     } else {
       postDecline(popup, state);
       cleanUpStorage();
     }
   };
+  const now = new Date();
+  // check if visitor has consented before
+  const cookieconsent = storageType.getItem("cookieconsent");
+  let { val, time } = JSON.parse(cookieconsent) || {};
 
-  const popup = d.getElementById("cookie-popup");
-  if (popup) {
-    const now = new Date();
+  w.onload = () => {
+    // if not consented or has expired
+    if (!cookieconsent || checkExpired(now, time, validConsentDuration)) {
+      const popup = new Popup(html_popup);
+      popup.show();
 
-    // check if visitor has consented before
-    const cookieconsent = storageType.getItem("cookieconsent");
-    let { val, time } = JSON.parse(cookieconsent) || {};
+      // user accepts
+      popup.get().querySelector("#ButtonCAccept").onclick = (e) => {
+        e.preventDefault();
+        handleClick(popup, true, now);
+        popup.close();
+      };
 
-    w.onload = () => {
-      // if not consented or has expired
-      if (!cookieconsent || checkExpired(now, time, validConsentDuration)) {
-        managePopup.show(popup);
+      // handle preferences
+      popup.get().querySelector("#ButtonCUpdate").onclick = (e) => {
+        e.preventDefault();
+        handleClick(popup, preferences.marketing, now);
+      };
 
-        // user accepts
-        popup.querySelector("#ButtonCAccept").onclick = (e) => {
-          e.preventDefault();
-          handleClick(popup, true, now);
-          managePopup.close(popup);
-        };
+      let toggles = popup.get().querySelectorAll(".cc__toggle--preference");
 
-        // handle preferences
-        popup.querySelector("#ButtonCUpdate").onclick = (e) => {
-          e.preventDefault();
-          handleClick(popup, preferences.marketing, now);
-        };
-      } else {
-        managePopup.remove(popup);
-        if (val) {
-          // run GTM Code
-          appendGtm(containerId);
-        } else {
-          // remove cookies
-          cleanUpStorage(domain);
-        }
-      }
-    };
-  }
-  let toggles = popup.querySelectorAll(".cc__toggle--preference");
-
-  toggles.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      btn.classList.toggle("cc__toggle--active");
-      updatePreference(state, btn.getAttribute("data-preference"));
-      Array.from(btn.children).forEach((node) => {
-        node.classList.toggle("cc__toggle--active");
+      toggles.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          btn.classList.toggle("cc__toggle--active");
+          updatePreference(state, btn.getAttribute("data-preference"));
+          Array.from(btn.children).forEach((node) => {
+            node.classList.toggle("cc__toggle--active");
+          });
+        });
       });
-    });
-  });
+    } else {
+      if (val) {
+        // run GTM Code
+        appendGtm(containerId);
+      } else {
+        // remove cookies
+        cleanUpStorage(domain);
+      }
+    }
+  };
 })(document, window);
