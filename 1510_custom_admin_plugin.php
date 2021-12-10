ceee96f24a03e5534cd6a2b281e94501aa965452
Will Leighton <will@fifteenten.co.uk>
2021-12-10 14:51:18 +0000

    plugin version

--- 1510_custom_admin_plugin.php
+++ 1510_custom_admin_plugin.php
@@ -35,7 +35,7 @@ register_deactivation_hook( __FILE__, 'deactivate_fifteenten_recruitment' );
 class FifteentenCustomAdmin_Plugin
 {  
 
-   public $version = "1.1.03";
+   public $version = "1.1.06";
 
    public function __construct()
    {
